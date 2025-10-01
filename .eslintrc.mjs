d19694a8fe5c1cdad3214213ff66fc1afa911e70
Jose Lawrence Aquino <joselawrenceaquino@CODY-MBP-76s-MacBook-Pro.local>
2025-10-02 00:15:00 +0800

    add additional reqrments package for fe

---
+++ .eslintrc.mjs
@@ -0,0 +1,13 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  rules: {
+    // Add custom rules here
+  }
+}
